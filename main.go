a2975256d1fc56959056b452bfc42f631e03bb27
Antonette Caldwell <pullmana8@gmail.com>
2022-06-02 14:46:34 -0500

    Grouped the structs on main.go and added routing to handle get and post.

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ type User struct {
 
 type FeedItem struct {
 	gorm.Model
-	ID        int    `json:"id" gorm:"primaryKey"`
+	ID        string `json:"id" gorm:"primaryKey"`
 	Email     User   `gorm:"embedded"`
 	Caption   string `json:"caption"`
 	Url       string `json:"url"`
@@ -29,16 +29,40 @@ type FeedItem struct {
 }
 
 var items = []FeedItem{
-	{ID: 1, Email: User{Email: "name@example.com"}, Caption: "Caption example 1", Url: "http://example.com/image.png", CreatedAt: time.Now(), UpdatedAt: time.Now()},
-	{ID: 2, Email: User{Email: "name@example.com"}, Caption: "Caption example 2", Url: "http://example.com/image.png", CreatedAt: time.Now(), UpdatedAt: time.Now()},
+	{ID: "1", Email: User{Email: "name@example.com"}, Caption: "Caption example 1", Url: "http://example.com/image.png", CreatedAt: time.Now(), UpdatedAt: time.Now()},
+	{ID: "2", Email: User{Email: "name@example.com"}, Caption: "Caption example 2", Url: "http://example.com/image.png", CreatedAt: time.Now(), UpdatedAt: time.Now()},
 }
 
 func getItems(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, items)
 }
 
+func postItems(c *gin.Context) {
+	var newItem FeedItem
+
+	if err := c.BindJSON(&newItem); err != nil {
+		return
+	}
+
+	items = append(items, newItem)
+	c.IndentedJSON(http.StatusCreated, newItem)
+}
+
+func getItemById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range items {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "item is not found"})
+}
 func main() {
 	r := gin.Default()
 	r.GET("/items", getItems)
+	r.GET("/items/:id", getItemById)
+	r.POST("/items", postItems)
 	r.Run()
 }
